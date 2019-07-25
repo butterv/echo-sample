e8c548f8c32f5b666878b585e2a03f53686169e8
istsh <github3104xniiapm@gmail.com>
2019-07-26 01:35:17 +0900

    Implement middleware

--- server.go
+++ server.go
@@ -4,12 +4,34 @@ import (
 	"net/http"
 	
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
+
+type User struct {
+	Name  string `json:"name" xml:"name" form:"name" query:"name"`
+	Email string `json:"email" xml:"email" form:"email" query:"email"`
+}
 
 func main() {
 	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	
+	e.Static("/static", "static")
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+
+	e.POST("/users", func(c echo.Context) error {
+		u := new(User)
+		if err := c.Bind(u); err != nil {
+			return err
+		}
+		return c.JSON(http.StatusCreated, u)
+		// or
+		// return c.XML(http.StatusCreated, u)
+	})
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
