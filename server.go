79bb81057cd1fa453e24a721e6e16a4715509010
istsh <github3104xniiapm@gmail.com>
2019-09-26 14:14:49 +0900

    Base echo server

--- server.go
+++ server.go
@@ -2,35 +2,15 @@ package main
 
 import (
 	"net/http"
-	
-	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
-)
 
-type User struct {
-	Name  string `json:"name" xml:"name" form:"name" query:"name"`
-	Email string `json:"email" xml:"email" form:"email" query:"email"`
-}
+	"github.com/labstack/echo"
+)
 
 func main() {
 	e := echo.New()
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-	
-	e.Static("/static", "static")
 
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
-	})
-
-	e.POST("/users", func(c echo.Context) error {
-		u := new(User)
-		if err := c.Bind(u); err != nil {
-			return err
-		}
-		return c.JSON(http.StatusCreated, u)
-		// or
-		// return c.XML(http.StatusCreated, u)
 	})
 
 	e.Logger.Fatal(e.Start(":1323"))
