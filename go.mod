b90d9a34b03b87316e3d2c50b705a7118f843364
istsh <github3104xniiapm@gmail.com>
2019-09-26 15:22:13 +0900

    WIP: labstack/echo sample

--- go.mod
+++ go.mod
@@ -1,8 +1,6 @@
 module github.com/istsh/go-echo
 
 require (
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
-	github.com/labstack/echo v3.3.10+incompatible // indirect
-	github.com/labstack/gommon v0.2.9 // indirect
-	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/labstack/echo/v4 v4.1.10
 )
--- server.go
+++ server.go
@@ -3,15 +3,43 @@ package main
 import (
 	"net/http"
 
-	"github.com/labstack/echo"
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
 
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+	//e.Pre(middleware.RemoveTrailingSlash())
+	//e.Use(middleware.RequestID())
+	//e.Use(middleware.Gzip())
+	//e.Use(middleware.Recover())
+	//e.Use(middleware.Secure())
+
+	v1 := e.Group("/v1")
+	groupV1(v1)
+	groupV1Users(v1)
 
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func groupV1(e *echo.Group) {
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "OK GET / (not a group)")
+	})
+	e.GET("/users/:id/profile", func(c echo.Context) error {
+		return c.String(http.StatusOK, "OK GET /users/:id/profile (not a group)")
+	})
+	e.DELETE("/users/:id", func(c echo.Context) error {
+		return c.String(http.StatusOK, "OK DELETE /users/:id (not a group)")
+	})
+}
+
+func groupV1Users(e *echo.Group) {
+	ug := e.Group("/users")
+	ug.GET("", func(c echo.Context) error {
+		return c.String(http.StatusOK, "OK GET /users")
+	})
+	ug.GET("/:id", func(c echo.Context) error {
+		return c.String(http.StatusOK, "OK GET /users/:id")
+	})
 }
